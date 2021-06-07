68b4795ef3755df6bb858ef5cc265d2933336a5e
khalil-sudo <72109796+khalil-sudo@users.noreply.github.com>
2021-06-07 03:00:18 +0100

    Added onSubmit functionality and used angular routing programmatically

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,6 +6,8 @@ import {
   FormBuilder,
 } from '@angular/forms';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -20,11 +22,18 @@ export class LoginComponent implements OnInit {
     email: ['', Validators.required],
     password: ['', Validators.required],
   });
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private rout: Router) {}
 
   ngOnInit(): void {}
 
   onSubmit() {
     console.warn(this.loginForm.value);
+
+    if (
+      this.loginForm.value.email == 'test@test.com' &&
+      this.loginForm.value.password == 'pass'
+    ) {
+      this.rout.navigate(['/home']);
+    } else alert('wrong email or password');
   }
 }
