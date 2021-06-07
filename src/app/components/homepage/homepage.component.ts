03d447d9c0e9dbdc81e6d1e51ce621f269e52b94
khalil-sudo <72109796+khalil-sudo@users.noreply.github.com>
2021-06-07 03:40:26 +0100

    adding homepage methods, about to delete todoSubmit() function

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
+  todoForm = this.fb.group({
+    todo: ['', Validators.required],
+  });
 
-  constructor() { }
+  todoArray: string[] = [];
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  addTodo() {
+    let value = this.todoForm.value.todo;
+    if (value != '') {
+      this.todoArray.push(value);
+      console.log(this.todoArray);
+      this.todoForm.reset();
+    } else {
+      alert('field required!');
+    }
+  }
+  deleteItem(todo: any) {
+    for (let i = 0; i < this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+        console.log('delete item');
+      }
+    }
   }
 
+  todoSubmit() {
+    let value = this.todoForm.value.todo;
+    if (value != '') {
+      console.log(value);
+      this.todoArray.push(value);
+      this.todoForm.reset();
+    } else {
+      alert('field required!');
+    }
+  }
 }
